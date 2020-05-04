3a23db7c241c4fb412b1e7a5c5d8d838edb33fcb
billal <billal.bou@gmail.com>
2020-05-04 11:37:30 +0200

    adds editor module (symbol overlay) and modify .gitignore

---
+++ modules/editor.el
@@ -0,0 +1,17 @@
+(use-package symbol-overlay
+  :diminish
+  :defines iedit-mode
+  :commands (symbol-overlay-get-symbol
+             symbol-overlay-assoc
+             symbol-overlay-get-list
+             symbol-overlay-jump-call)
+  :bind (("M-i" . symbol-overlay-put)
+         ("M-n" . symbol-overlay-jump-next)
+         ("M-p" . symbol-overlay-jump-prev)
+         ("M-N" . symbol-overlay-switch-forward)
+         ("M-P" . symbol-overlay-switch-backward)
+         ("M-C" . symbol-overlay-remove-all)
+         ([M-f3] . symbol-overlay-remove-all))
+  :hook ((prog-mode . symbol-overlay-mode)
+         (iedit-mode . (lambda () (symbol-overlay-mode -1)))
+         (iedit-mode-end . symbol-overlay-mode)))
